fbe7e2dbeb2c46661c70a3a03c21a0a70c389f13
Yash Totale <totaleyash@gmail.com>
2022-10-15 22:01:01 -0700

    Add Background Images to Header

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -31,16 +31,6 @@ const Header: FC = () => {
     >
       <Navbar isSizeMedium={isSizeMedium} />
       <Hero isSizeSmall={isSizeSmall} isSizeMedium={isSizeMedium} />
-      <img
-        className="background-item-1"
-        src="images/background-item-1.svg"
-        alt=""
-      />
-      <img
-        className="background-item-2"
-        src="images/background-item-2.svg"
-        alt=""
-      />
     </header>
   );
 };
@@ -52,85 +42,114 @@ interface HeroProps {
 
 const Hero: FC<HeroProps> = ({ isSizeSmall, isSizeMedium }) => {
   return (
-    <div
-      css={{
-        textAlign: isSizeSmall ? "center" : "start",
-      }}
-    >
+    <>
       <div
-        css={
-          isSizeSmall
-            ? {}
-            : { ...grid, ...grid1x2, ...gridCentered, ...gridGapSmall }
-        }
+        css={{
+          textAlign: isSizeSmall ? "center" : "start",
+        }}
       >
-        <div>
-          <h1
-            css={{
-              ...h1,
-              marginBottom: "2rem",
-            }}
-          >
-            Discover & Invest in Curated Art!
-          </h1>
-          <p
-            css={{
-              marginBottom: "3.2rem",
-              ...text,
-              ...textSmall,
-              ...textRegular,
-            }}
-          >
-            Artful is a decentralized physical art marketplace that empowers the
-            creator economy.
-          </p>
-          <button
-            css={{
-              width: isSizeMedium ? undefined : "23rem",
-              ...btn,
-              ...btnFlex,
-              ...btnAccent,
-            }}
-          >
-            <span
+        <div
+          css={
+            isSizeSmall
+              ? {}
+              : { ...grid, ...grid1x2, ...gridCentered, ...gridGapSmall }
+          }
+        >
+          <div>
+            <h1
               css={{
-                ...textStandard,
-                ...textMedium,
+                ...h1,
+                marginBottom: "2rem",
               }}
             >
-              Try for FREE
-            </span>
-            <div
+              Discover & Invest in Curated Art!
+            </h1>
+            <p
               css={{
-                display: "inline-flex",
-                width: "3.2rem",
-                height: "3.2rem",
-                justifyContent: "center",
-                alignItems: "center",
-                backgroundColor: colorWhite,
-                borderRadius: "100%",
-                WebkitBorderRadius: "100%",
-                MozBorderRadius: "100%",
-                MsBorderRadius: "100%",
-                OBorderRadius: "100%",
-                marginLeft: "2.4rem",
+                marginBottom: "3.2rem",
+                ...text,
+                ...textSmall,
+                ...textRegular,
               }}
             >
-              <img src="images/chevron-right.svg" alt="Right Icon" />
-            </div>
-          </button>
+              Artful is a decentralized physical art marketplace that empowers
+              the creator economy.
+            </p>
+            <button
+              css={{
+                width: isSizeMedium ? undefined : "23rem",
+                ...btn,
+                ...btnFlex,
+                ...btnAccent,
+              }}
+            >
+              <span
+                css={{
+                  ...textStandard,
+                  ...textMedium,
+                }}
+              >
+                Try for FREE
+              </span>
+              <div
+                css={{
+                  display: "inline-flex",
+                  width: "3.2rem",
+                  height: "3.2rem",
+                  justifyContent: "center",
+                  alignItems: "center",
+                  backgroundColor: colorWhite,
+                  borderRadius: "100%",
+                  WebkitBorderRadius: "100%",
+                  MozBorderRadius: "100%",
+                  MsBorderRadius: "100%",
+                  OBorderRadius: "100%",
+                  marginLeft: "2.4rem",
+                }}
+              >
+                <img src="images/chevron-right.svg" alt="Right Icon" />
+              </div>
+            </button>
+          </div>
+          <img
+            src="images/art-museum.svg"
+            alt="Art Museum"
+            css={{
+              display: isSizeSmall ? "none" : "block",
+            }}
+            data-rellax-horizontal-speed="-2"
+            data-rellax-vertical-scroll-axis="x"
+          />
         </div>
-        <img
-          src="images/art-museum.svg"
-          alt="Art Museum"
-          css={{
-            display: isSizeSmall ? "none" : "block",
-          }}
-          data-rellax-horizontal-speed="-2"
-          data-rellax-vertical-scroll-axis="x"
-        />
       </div>
-    </div>
+      {!isSizeSmall && (
+        <>
+          <img
+            css={{
+              width: "50%",
+              position: "absolute",
+              bottom: "0",
+              right: "50%",
+              zIndex: "0",
+              pointerEvents: "none",
+            }}
+            src="images/background-item-1.svg"
+            alt=""
+          />
+          <img
+            css={{
+              width: "50%",
+              position: "absolute",
+              bottom: "10%",
+              right: "5%",
+              pointerEvents: "none",
+            }}
+            src="images/background-item-2.svg"
+            alt=""
+          />
+        </>
+      )}
+    </>
   );
 };
 
